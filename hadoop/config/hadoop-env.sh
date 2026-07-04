7b0b95652b49ec066f8133716e0ad039dd486efd
Bilal <bilalaloui00@gmail.com>
2026-07-04 18:29:34 +0200

    fix(docker): fix Hive startup avec bde2020/hive et config Hadoop

---
+++ hadoop/config/hadoop-env.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.212.b04-0.el7_6.x86_64/jre}
+export HADOOP_HOME=/opt/hadoop
+export HADOOP_CONF_DIR=/opt/hadoop/etc/hadoop
+export HADOOP_LOG_DIR=/opt/hadoop/logs
